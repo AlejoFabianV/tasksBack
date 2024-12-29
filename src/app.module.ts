fa2f252701d8a840ede2db5c69facf1771cae78d
AlejoFabianV <verhagenalejo@gmail.com>
2024-12-29 20:05:38 -0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     TasksModule,
-    MongooseModule.forRoot('mongodb+srv://verhagenalejo:Independiente28-@cluster0.pd4yr.mongodb.net/api/tasks'),
+    MongooseModule.forRoot('mongodb+srv://verhagenalejo:Independiente28-@cluster0.pd4yr.mongodb.net'),
     ],
 })
 export class AppModule {}
